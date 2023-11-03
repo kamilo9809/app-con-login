f6e7db74247e6c1959b2bc3b33225925eb94cb98
Branlopez30 <131416245+Branlopez30@users.noreply.github.com>
2023-11-03 16:26:11 -0500

    Update form.jsx
    formulario

--- src/pages/components/form.jsx
+++ src/pages/components/form.jsx
@@ -1,9 +1,42 @@
 
 const Form = () => {
   return (
-    <div>
+    <form>
+      <h1>Pregunta 1</h1>
+      {/*Pregunta*/}
+      <label 
+      htmlFor="Pregunta">Titulo de la pregunta</
+      label>
+      <input type="text" />
+
+      {/*Seleccionar pregunta*/}
+      <label 
+      htmlFor="Seleccionar pregunta">Seleccionar Pregunta</label>
+      <select>
+        <option
+        value="abi">Abierta</option>
+        <option
+        value="cer">Cerrada</option>
+      </select>
+
+      {/*Descripcion pregunta*/}
+      <label 
+      htmlFor="Descripcion">Descripcion de la pregunta</
+      label>
+      <input type="text" />
+
+
+      {/*Opcion cerrada*/}
+      <label 
+      htmlFor="Descripcion">Cerrada</
+      label>
+      <input type="checkbox" />
+
+
+      <button>Siguiente</button>
       
-    </div>
+      
+    </form>
   )
 }
 
